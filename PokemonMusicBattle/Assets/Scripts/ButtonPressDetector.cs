8c3ff80c2ba00fdd0d11f9e20a8a7aeb5efc2058
Edward Lu <edwardzlu98@gmail.com>
2020-12-29 14:06:01 -0500

    Added message display logic

--- PokemonMusicBattle/Assets/Scripts/ButtonPressDetector.cs
+++ PokemonMusicBattle/Assets/Scripts/ButtonPressDetector.cs
@@ -24,12 +24,18 @@ public class ButtonPressDetector : MonoBehaviour
     private bool inputIndicatorShouldBlink = true;
     private bool inputIndicatorVisible = true;
 
+    private bool readyForNextAction = false;
+
     // Start is called before the first frame update
     void Start()
     {
         toggleMenu1Buttons(true);
         toggleMenu2Buttons(false);
         StartCoroutine("InputIndicatorBlinker");
+
+        readyForNextAction = false;
+        IEnumerator initialMessage = DisplayMessage("What will PIKACHU do?");
+        StartCoroutine(initialMessage);
     }
 
     // Update is called once per frame
@@ -162,5 +168,195 @@ public class ButtonPressDetector : MonoBehaviour
     {
         menuAnimator.Play("BattleEventsPlayed");
         enableMenu(2);
+    }
+
+    IEnumerator DisplayMessage(string message)
+    {
+        for (int i = 1; i < message.Length + 1; i++)
+        {
+            GameObject.Find("text" + i).GetComponent<SpriteRenderer>().sprite = null;
+        }
+        for (int i = 1; i < message.Length + 1; i++)
+        {
+            yield return new WaitForSeconds(.05f);
+            string symbolFileName = "";
+            char symbol = message[i - 1];
+            switch (symbol)
+            {
+                case 'a':
+                    symbolFileName = "al";//.gif";";
+                    break;
+                case 'b':
+                    symbolFileName = "bl";//.gif";";
+                    break;
+                case 'c':
+                    symbolFileName = "cl";//.gif";";
+                    break;
+                case 'd':
+                    symbolFileName = "dl";//.gif";";
+                    break;
+                case 'e':
+                    symbolFileName = "el";//.gif";";
+                    break;
+                case 'f':
+                    symbolFileName = "fl";//.gif";";
+                    break;
+                case 'g':
+                    symbolFileName = "gl";//.gif";";
+                    break;
+                case 'h':
+                    symbolFileName = "hl";//.gif";";
+                    break;
+                case 'i':
+                    symbolFileName = "il";//.gif";";
+                    break;
+                case 'j':
+                    symbolFileName = "jl";//.gif";";
+                    break;
+                case 'k':
+                    symbolFileName = "kl";//.gif";";
+                    break;
+                case 'l':
+                    symbolFileName = "ll";//.gif";";
+                    break;
+                case 'm':
+                    symbolFileName = "ml";//.gif";";
+                    break;
+                case 'n':
+                    symbolFileName = "nl";//.gif";";
+                    break;
+                case 'o':
+                    symbolFileName = "ol";//.gif";";
+                    break;
+                case 'p':
+                    symbolFileName = "pl";//.gif";";
+                    break;
+                case 'q':
+                    symbolFileName = "ql";//.gif";";
+                    break;
+                case 'r':
+                    symbolFileName = "rl";//.gif";";
+                    break;
+                case 's':
+                    symbolFileName = "sl";//.gif";";
+                    break;
+                case 't':
+                    symbolFileName = "tl";//.gif";";
+                    break;
+                case 'u':
+                    symbolFileName = "ul";//.gif";";
+                    break;
+                case 'v':
+                    symbolFileName = "vl";//.gif";";
+                    break;
+                case 'w':
+                    symbolFileName = "wl";//.gif";";
+                    break;
+                case 'x':
+                    symbolFileName = "xl";//.gif";";
+                    break;
+                case 'y':
+                    symbolFileName = "yl";//.gif";";
+                    break;
+                case 'z':
+                    symbolFileName = "zl";//.gif";";
+                    break;
+                case 'A':
+                    symbolFileName = "A";//.gif";";
+                    break;
+                case 'B':
+                    symbolFileName = "B";//.gif";";
+                    break;
+                case 'C':
+                    symbolFileName = "C";//.gif";";
+                    break;
+                case 'D':
+                    symbolFileName = "D";//.gif";";
+                    break;
+                case 'E':
+                    symbolFileName = "E";//.gif";";
+                    break;
+                case 'F':
+                    symbolFileName = "F";//.gif";";
+                    break;
+                case 'G':
+                    symbolFileName = "G";//.gif";";
+                    break;
+                case 'H':
+                    symbolFileName = "H";//.gif";";
+                    break;
+                case 'I':
+                    symbolFileName = "I";//.gif";";
+                    break;
+                case 'J':
+                    symbolFileName = "J";//.gif";";
+                    break;
+                case 'K':
+                    symbolFileName = "K";//.gif";";
+                    break;
+                case 'L':
+                    symbolFileName = "L";//.gif";";
+                    break;
+                case 'M':
+                    symbolFileName = "M";//.gif";";
+                    break;
+                case 'N':
+                    symbolFileName = "N";//.gif";";
+                    break;
+                case 'O':
+                    symbolFileName = "O";//.gif";";
+                    break;
+                case 'P':
+                    symbolFileName = "P";//.gif";";
+                    break;
+                case 'Q':
+                    symbolFileName = "Q";//.gif";";
+                    break;
+                case 'R':
+                    symbolFileName = "R";//.gif";";
+                    break;
+                case 'S':
+                    symbolFileName = "S";//.gif";";
+                    break;
+                case 'T':
+                    symbolFileName = "T";//.gif";";
+                    break;
+                case 'U':
+                    symbolFileName = "U";//.gif";";
+                    break;
+                case 'V':
+                    symbolFileName = "V";//.gif";";
+                    break;
+                case 'W':
+                    symbolFileName = "W";//.gif";";
+                    break;
+                case 'X':
+                    symbolFileName = "X";//.gif";";
+                    break;
+                case 'Y':
+                    symbolFileName = "Y";//.gif";";
+                    break;
+                case 'Z':
+                    symbolFileName = "Z";//.gif";";
+                    break;
+                case '!':
+                    symbolFileName = "!";//.png";
+                    break;
+                case '?':
+                    symbolFileName = "questionmark";//.png";
+                    break;
+                case ' ':
+                    symbolFileName = "";
+                    break;
+                default:
+                    symbolFileName = "!";//.png";
+                    break;
+            }
+            string textSlotName = "text" + i;
+            string symbolFilePath = "font/" + symbolFileName;
+            Debug.Log("Setting text slot " + textSlotName + " to symbol path " + symbolFilePath);
+            GameObject.Find(textSlotName).GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(symbolFilePath);
+        }
+        readyForNextAction = true;
     }
 }
