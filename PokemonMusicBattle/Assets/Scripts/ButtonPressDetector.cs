62c95f9ab95b3e55142b9073747827de5fe24efd
Edward Lu <edwardzlu98@gmail.com>
2020-12-28 20:29:24 -0500

    Initial commit

---
+++ PokemonMusicBattle/Assets/Scripts/ButtonPressDetector.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressDetector : MonoBehaviour
+{
+
+    public Animator menuAnimator;
+    public AudioSource buttonSoundPlayer;
+    public AudioClip buttonPressSound;
+    public AudioClip buttonRejectSound;
+    public GameObject pikachuSprite;
+
+    public Button fightButton;
+    public Button bagButton;
+    public Button runButton;
+    public Button pokemonButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleMenu1Buttons(bool enabled)
+    {
+        fightButton.enabled = enabled;
+        bagButton.enabled = enabled;
+        runButton.enabled = enabled;
+        pokemonButton.enabled = enabled;
+    }
+
+    public void toggleMenu2Buttons(bool enabled)
+    {
+    }
+
+    // Menu 1 buttons
+    //*******************************************//
+    public void FightButtonPressed()
+    {
+        Debug.Log("Fight button pressed.");
+        menuAnimator.Play("FightPressing");
+        buttonSoundPlayer.PlayOneShot(buttonPressSound);
+        pikachuSprite.GetComponent<SpriteRenderer>().enabled = false;
+        toggleMenu1Buttons(false);
+        toggleMenu2Buttons(true);
+    }
+    public void BagButtonPressed()
+    {
+        Debug.Log("Bag button pressed.");
+        buttonSoundPlayer.PlayOneShot(buttonRejectSound);
+    }
+    public void RunButtonPressed()
+    {
+        Debug.Log("Run button pressed.");
+        buttonSoundPlayer.PlayOneShot(buttonRejectSound);
+    }
+    public void PokemonButtonPressed()
+    {
+        Debug.Log("Pokemon button pressed.");
+        buttonSoundPlayer.PlayOneShot(buttonRejectSound);
+    }
+    //*******************************************//
+
+}
